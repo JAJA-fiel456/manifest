bd8a9badb9bfb176a5c18b156a0b9a97bbbe858f
JAJA-fiel456 <joaojacomo341@gmail.com>
2026-09-10 21:11:55 +0000

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	deleted:    "Aula 02 - Web App Manifest e instalac\314\247a\314\203o de PWAs.pdf"
    	modified:   app.js
    	renamed:    icon-192.png -> icons/icon-192.png
    	renamed:    icon-512.png -> icons/icon-512.png
    	modified:   index.html
    	deleted:    main.html
    	modified:   manifest.webmanifest
    	modified:   style.css
    	new file:   sw.js

---
+++ sw.js
@@ -0,0 +1,15 @@
+self.addEventListener("install", event => {
+    console.log("Service Worker instalado");
+});
+
+self.addEventListener("activate", event => {
+    console.log("Service Worker ativado");
+});
+
+self.addEventListener("fetch", event => {
+    console.log("URL:");
+    console.log(event.request.url);
+
+    console.log("Método:");
+    console.log(event.request.method);
+});
